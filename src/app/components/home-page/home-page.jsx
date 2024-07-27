5a4a6bdb988ebe3d56ff294de6734d5561bbe676
Shane T. Geary <shane.geary@glowstik.com>
2024-07-27 17:22:44 -0600

    New font

--- src/app/components/home-page/home-page.jsx
+++ src/app/components/home-page/home-page.jsx
@@ -1,6 +1,10 @@
+// import {useState} from 'react'
+
 import styles from './home-page.module.css'
 
 export const HomePage = () => {
+	// const [downloadButtonClicked, setDownloadButtonClicked] = useState(false)
+
 	return (
 		<div className={styles['heroSection']}>
 			<div className={styles['heroLeft']}>
@@ -10,7 +14,12 @@ export const HomePage = () => {
 			</div>
 			<div className={styles['heroRight']}>
 				<div className={styles['buttonContainer']}>
-					<button className={styles['downloadButton']}>
+					<button
+						className={styles['downloadButton']}
+						// onMouseUp={() => {
+
+						// }}
+					>
 						<span>
 							<div className={styles['downloadIcon']}>
 								<div className={styles['arrow']}></div>
