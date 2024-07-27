380ac4bc28088a87f316dac995151774cd4dc5f8
Shane T. Geary <shane.geary@glowstik.com>
2024-07-27 14:12:09 -0600

    WIP donwload resume button on home page

--- src/app/components/home-page/home-page.jsx
+++ src/app/components/home-page/home-page.jsx
@@ -1,13 +1,30 @@
+import Image from 'next/image'
+
 import styles from './home-page.module.css'
+
+import downloadIcon from '../../../../public/download-icon.png'
 
 export const HomePage = () => {
 	return (
-		<div className={styles.heroSection}>
-			<div className={styles.nameContainer}>
-				<h1 className={styles.heroTitle}>Shane T Geary</h1>
+		<div className={styles['heroSection']}>
+			<div className={styles['heroLeft']}>
+				<h1 className={styles['heroTitle']}>
+					Shane T<br /> Geary
+				</h1>
 			</div>
-			<div className={styles.buttonContainer}>
-				<button className={styles.downloadButton}>Download Resume</button>
+			<div className={styles['heroRight']}>
+				<div className={styles['buttonContainer']}>
+					<button className={styles['downloadButton']}>
+						<span>
+							<div className={styles['downloadIcon']}>
+								<div className={styles['arrow']}></div>
+								<div className={styles['line']}></div>
+								<div className={styles['base']}></div>
+							</div>
+							Resume
+						</span>
+					</button>
+				</div>
 			</div>
 		</div>
 	)
