ef97d1a40e085d5bf9ee4ac9c52a468964e6fc1b
Shane T. Geary <shane.geary@glowstik.com>
2024-07-28 11:09:05 -0600

    Added content and adjusted styles

--- src/app/components/about-me/about-me.jsx
+++ src/app/components/about-me/about-me.jsx
@@ -7,7 +7,21 @@ export const AboutMe = () => {
 			<div className={styles['aboutMeText']}>
 				Software Engineer experienced in ReactJS/NextJS/Typescript, HTML, CSS
 				and other frontend languages and technologies. Also experienced in and
-				geared towards cloud computing and IT, specializing in AWS.
+				geared towards cloud computing and IT, specializing in AWS. With 3 years
+				tech startup experience, I have a proven track record of innovative
+				software solutions.
+				<br />
+				<br />
+				When I am not coding, I enjoy my love for music, both listening and
+				playing. Growing up in a musical family, I had learned several
+				instruments by the time I turned 18. I also found a love for electonic
+				music production and sound design. Which crosses over nicely into my
+				love for tech. An interesting fact about me, during my younder adult
+				years, I found an interest and affinity for a sub-culture art called
+				"Gloving". This art form is a dance style that uses LED gloves to create
+				visual effects to music. I have performed at many music events and
+				festivals in Colorado, both as a solo performer and as part of street
+				teams.
 			</div>
 		</div>
 	)
