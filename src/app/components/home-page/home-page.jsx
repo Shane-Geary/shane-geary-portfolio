acca676be779c4536712b4c385dde2c538d6ff8c
Shane T. Geary <shane.geary@glowstik.com>
2024-08-01 16:55:45 -0600

    Using swiper.js for mobile HomePage sections. Override Swiper Navigation arrow styles

--- src/app/components/home-page/home-page.jsx
+++ src/app/components/home-page/home-page.jsx
@@ -7,29 +7,92 @@ import {AboutMe} from '../about-me'
 import headshot from '../../../../public/shane-headshot.jpeg'
 
 import classNames from 'classnames'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper'
 
 import styles from './home-page.module.scss'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 export const HomePage = () => {
 	const [toggleSection, setToggleSection] = useState(false)
+	const [activeIndex, setActiveIndex] = useState(0)
 
 	return (
 		<div className={styles['homePage']}>
-			<div
-				className={classNames(styles['heroSection'], {
-					[styles['heroSectionToggle']]: toggleSection
-				})}
+			<Swiper
+				direction='vertical'
+				autoplay={false}
+				spaceBetween={50}
+				slidesPerView={1}
+				navigation
+				pagination={{clickable: true}}
+				// scrollbar={{draggable: true}}
+				style={{height: '100vh', position: 'relative'}}
+				// allowSlideNext={false}
+				// allowSlidePrev={false}
+				Navigation={{
+					nextEl: `className=${styles['swiper-button-next']}`,
+					prevEl: `className=${styles['swiper-button-prev']}`
+				}}
 			>
-				<div className={styles['heroLeft']}>
-					<h1 className={styles['heroTitle']}>
-						Shane T<br /> Geary
-					</h1>
-				</div>
-				<div className={styles['heroRight']}>
-					<ResumeButton />
-				</div>
-			</div>
-			<button
+				<SwiperSlide className={styles['slideOneWrapper']}>
+					<div
+						className={classNames(styles['heroSection'], {
+							[styles['heroSectionToggle']]: toggleSection
+						})}
+					>
+						<div className={styles['heroLeft']}>
+							<h1 className={styles['heroTitle']}>
+								Shane T<br /> Geary
+							</h1>
+						</div>
+						<div className={styles['heroRight']}>
+							<ResumeButton />
+						</div>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className={styles['slideTwoWrapper']}>
+					<div
+						className={classNames(styles['aboutMeContainer'], {
+							[styles['aboutMeContainerToggle']]: toggleSection
+						})}
+					>
+						<h2 className={styles['aboutMeTitle']}>About Me</h2>
+						<div className={styles['headshotPositionWrapper']}>
+							<div className={styles['headshotContainer']}>
+								<Image
+									src={headshot}
+									alt='Shane Headshot'
+									width={580}
+									height={580}
+									layout='responsive'
+									style={{
+										borderRadius: '50%'
+									}}
+								/>
+							</div>
+						</div>
+						<div className={styles['aboutMePositionWrapper']}>
+							<AboutMe />
+						</div>
+					</div>
+				</SwiperSlide>
+			</Swiper>
+			<div
+				className={
+					styles['navigationArrow'] + ' ' + styles['navigationArrowNext']
+				}
+			/>
+			<div
+				className={
+					styles['navigationArrow'] + ' ' + styles['navigationArrowPrev']
+				}
+			/>
+			{/* <button
 				className={classNames(styles['downArrowIcon'], {
 					[styles['downArrowIconOpen']]: toggleSection
 				})}
@@ -45,31 +108,7 @@ export const HomePage = () => {
 				onClick={() => {
 					setToggleSection(!toggleSection)
 				}}
-			/>
-			<div
-				className={classNames(styles['aboutMeContainer'], {
-					[styles['aboutMeContainerToggle']]: toggleSection
-				})}
-			>
-				<h2 className={styles['aboutMeTitle']}>About Me</h2>
-				<div className={styles['headshotPositionWrapper']}>
-					<div className={styles['headshotContainer']}>
-						<Image
-							src={headshot}
-							alt='Shane Headshot'
-							width={580}
-							height={580}
-							layout='responsive'
-							style={{
-								borderRadius: '50%'
-							}}
-						/>
-					</div>
-				</div>
-				<div className={styles['aboutMePositionWrapper']}>
-					<AboutMe />
-				</div>
-			</div>
+			/> */}
 		</div>
 	)
 }
