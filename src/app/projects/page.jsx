28f1adbe9196e33599a0b646cd5d2176ab561639
Shane T. Geary <shane.geary@glowstik.com>
2024-08-07 16:54:26 -0600

    Working on Projects section

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,3 +1,45 @@
+import Image from 'next/image'
+
+import GlowstikFontLogo from '../../../public/Glowstik_Logo_Door.svg'
+import GlowstikLogo from '../../../public/Glowstik_Logo.svg'
+
+import styles from './projects.module.scss'
+
 export default function Projects() {
-	return <div>What up from the projects page</div>
+	return (
+		<div className={styles['projectsContainer']}>
+			<div className={styles['logoPositionContainer']}>
+				<div className={styles['glowstikFontLogoWrapper']}>
+					<Image
+						src={GlowstikFontLogo}
+						width={1100}
+						height={260}
+						layout='responsive'
+					/>
+				</div>
+			</div>
+			<div className={styles['glowstikTextSection']}>
+				<div className={styles['glowstikTextContainer']}>
+					<div className={styles['contentContainer']}>
+						<div className={styles['glowstikLogoWrapper']}>
+							<Image
+								src={GlowstikLogo}
+								width={1024}
+								height={1024}
+								layout='responsive'
+							/>
+						</div>
+						<div className={styles['glowstikText']}>
+							Glowstiks patented technology cloaks your location and doesn’t
+							share personal information. As a result, all the opportunities are
+							matched and shown on a map so people can go to areas of
+							opportunity. People can safely broadcast messages in real-time to
+							other people in their area, so they can find anything, sell
+							anything, meet anyone or share anything.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
