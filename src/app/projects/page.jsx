8b8e86aac85d8cb4d45ee8cb704eedf147a66399
Shane T. Geary <shane.geary@glowstik.com>
2024-08-07 19:51:19 -0600

    Projects section title

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -9,44 +9,47 @@ import styles from './projects.module.scss'
 export default function Projects() {
 	return (
 		<div className={styles['projectsContainer']}>
-			<div className={styles['logoPositionContainer']}>
-				<div className={styles['glowstikFontLogoWrapper']}>
-					<Image
-						src={GlowstikFontLogo}
-						width={1100}
-						height={260}
-						layout='responsive'
-					/>
-				</div>
-			</div>
-			<div className={styles['glowstikTextSection']}>
-				<Link
-					className={styles['glowstikUrlWrapper']}
-					href={'https://www.glowstik.com/'}
-					target='_blank'
-				>
-					https://www.glowstik.com/
-				</Link>
-				<div className={styles['glowstikTextContainer']}>
-					<div className={styles['glowstikLogoWrapper']}>
+			<div className={styles['projectsTitleWrapper']}>Projects</div>
+			<div className={styles['glowstikProjectContainer']}>
+				<div className={styles['logoPositionContainer']}>
+					<div className={styles['glowstikFontLogoWrapper']}>
 						<Image
-							src={GlowstikLogo}
-							width={1024}
-							height={1024}
+							src={GlowstikFontLogo}
+							width={1100}
+							height={260}
 							layout='responsive'
 						/>
 					</div>
-					<div className={styles['glowstikText']}>
-						Glowstiks patented technology cloaks your location and doesn’t share
-						personal information. As a result, all the opportunities are matched
-						and shown on a map so people can go to areas of opportunity. People
-						can safely broadcast messages in real-time to other people in their
-						area, so they can find anything, sell anything, meet anyone or share
-						anything.
-					</div>
 				</div>
-				<div className={styles['projectTitleWrapper']}>
-					Software Engineer - Founding Team
+				<div className={styles['glowstikTextSection']}>
+					<Link
+						className={styles['glowstikUrlWrapper']}
+						href={'https://www.glowstik.com/'}
+						target='_blank'
+					>
+						https://www.glowstik.com/
+					</Link>
+					<div className={styles['glowstikTextContainer']}>
+						<div className={styles['glowstikLogoWrapper']}>
+							<Image
+								src={GlowstikLogo}
+								width={1024}
+								height={1024}
+								layout='responsive'
+							/>
+						</div>
+						<div className={styles['glowstikText']}>
+							Glowstiks patented technology cloaks your location and doesn’t
+							share personal information. As a result, all the opportunities are
+							matched and shown on a map so people can go to areas of
+							opportunity. People can safely broadcast messages in real-time to
+							other people in their area, so they can find anything, sell
+							anything, meet anyone or share anything.
+						</div>
+					</div>
+					<div className={styles['projectTitleWrapper']}>
+						Software Engineer - Founding Team
+					</div>
 				</div>
 			</div>
 		</div>
