0a51c4d41ce279ecfa921345d7676a9fe9506180
Shane T. Geary <shane.geary@glowstik.com>
2024-08-08 12:04:03 -0600

    Swiper slides and nav arrows on qualifications page

--- src/app/qualifications/page.jsx
+++ src/app/qualifications/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import Image from 'next/image'
 
 import styles from './qualifications.module.scss'
@@ -16,25 +16,55 @@ import FlatironCertificate from '../../../public/shane-flatiron-certificate.JPG'
 export default function Qualifications() {
 	const [imageFullscreen, setImageFullscreen] = useState(false)
 
+	const nextArrowRef = useRef(null)
+	const prevArrowRef = useRef(null)
+
 	return (
 		<div className={styles['qualificationsWrapper']}>
 			<div className={styles['qualificationsTitleWrapper']}>Qualifications</div>
 			<Swiper
 				modules={[Navigation]}
+				direction='vertical'
 				autoplay={false}
 				spaceBetween={10}
 				slidesPerView={1}
-				navigation
 				speed={500}
 				style={{height: '100vh', position: 'relative'}}
+				navigation={{
+					nextEl: '#swiperButtonNextID',
+					prevEl: '#swiperButtonPrevID'
+				}}
+				enabled
+				onSwiper={() => {
+					nextArrowRef.current = document.getElementById('swiperButtonNextID')
+					prevArrowRef.current = document.getElementById('swiperButtonPrevID')
+
+					prevArrowRef.current.style.transition = 'opacity 0.3s'
+					nextArrowRef.current.style.transition = 'opacity 0.3s'
+
+					prevArrowRef.current.style.opacity = 0
+				}}
+				onSlideChange={(e) => {
+					if (e.activeIndex === 0) {
+						prevArrowRef.current.style.opacity = 0
+						nextArrowRef.current.style.opacity = 1
+					} else {
+						prevArrowRef.current.style.opacity = 1
+						nextArrowRef.current.style.opacity = 0
+					}
+				}}
 			>
-				<SwiperSlide>
+				<SwiperSlide className={styles['flatironSlide']}>
 					<div className={styles['flatironContainer']}>
 						<div className={styles['flatironTextSection']}>
-							<div className={styles['flatironTitleWrapper']}>
+							{/* <div className={styles['flatironTitleWrapper']}>
 								Software Engineering Certificate
+							</div> */}
+							<div className={styles['flatironText']}>
+								Rigorous coding bootcamp where I learned the fundamentals of web
+								development languages. Such as HTML, CSS, JavaScript, ReactJS,
+								etc.
 							</div>
-							<div className={styles['flatironText']}>Flatiron School...</div>
 						</div>
 						<div
 							className={classNames(styles['flatironCertImageWrapper'], {
@@ -53,6 +83,19 @@ export default function Qualifications() {
 						</div>
 					</div>
 				</SwiperSlide>
+				<SwiperSlide>
+					<div>YO</div>
+				</SwiperSlide>
+				<div
+					ref={prevArrowRef}
+					id='swiperButtonPrevID'
+					className={styles['swiperButtonPrev']}
+				/>
+				<div
+					ref={nextArrowRef}
+					id='swiperButtonNextID'
+					className={styles['swiperButtonNext']}
+				/>
 			</Swiper>
 		</div>
 	)
