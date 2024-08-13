c1afc3882979aa6df226a34dbaaa1514c857761f
Shane T. Geary <shane.geary@glowstik.com>
2024-08-13 14:55:29 -0600

    Logic to mimic hover effect of ResumeButton on touch devices click events

--- src/components/resume-button/resume-button.jsx
+++ src/components/resume-button/resume-button.jsx
@@ -8,22 +8,42 @@ import styles from './resume-button.module.scss'
 export const ResumeButton = () => {
 	const [downloadButtonClicked, setDownloadButtonClicked] = useState(false)
 
+	const handleButtonClick = () => {
+		setDownloadButtonClicked(true)
+
+		const button = document.querySelector(`.${styles['downloadButton']}`)
+		button.classList.add(styles['hoverEffect'])
+
+		setTimeout(() => {
+			button.classList.remove(styles['hoverEffect'])
+			setDownloadButtonClicked(false)
+		}, 750) // 750ms to match the transition duration
+
+		const a = document.createElement('a')
+		a.href = '/Shane-Geary-Resume.pdf'
+		a.download = 'Shane-Geary-Resume.pdf'
+		a.click()
+		a.remove()
+		console.log('Download button clicked')
+	}
+
 	return (
 		<div className={styles['buttonContainer']}>
 			<button
 				className={styles['downloadButton']}
-				onClick={() => {
-					setDownloadButtonClicked(true)
-					const a = document.createElement('a')
-					a.href = '/Shane-Geary-Resume.pdf'
-					a.download = 'Shane-Geary-Resume.pdf'
-					a.click()
-					a.remove()
-					setTimeout(() => {
-						setDownloadButtonClicked(false)
-					}, 2000)
-					console.log('Download button clicked')
-				}}
+				onClick={handleButtonClick}
+				// onClick={() => {
+				// 	setDownloadButtonClicked(true)
+				// 	const a = document.createElement('a')
+				// 	a.href = '/Shane-Geary-Resume.pdf'
+				// 	a.download = 'Shane-Geary-Resume.pdf'
+				// 	a.click()
+				// 	a.remove()
+				// 	setTimeout(() => {
+				// 		setDownloadButtonClicked(false)
+				// 	}, 2000)
+				// 	console.log('Download button clicked')
+				// }}
 			>
 				<span>
 					<div className={styles['downloadIcon']}>
