e239796c8737f42449459103ca8c6e6a2641f8ae
Shane T. Geary <shane.geary@glowstik.com>
2024-08-14 14:33:13 -0600

    Create Cloudfront distribution resource with the S3 bucket as its origin

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,17 +1,34 @@
 import * as cdk from 'aws-cdk-lib'
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 import {Construct} from 'constructs'
 
 export class CdkStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props)
 
-		new s3.Bucket(this, 'PersonalWebsiteBucket', {
+		const bucket = new s3.Bucket(this, 'PersonalWebsiteBucket', {
 			bucketName: `personal-website-bucket-${cdk.Aws.ACCOUNT_ID}`,
-			publicReadAccess: false,
+			publicReadAccess: true,
+			blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
 			removalPolicy: cdk.RemovalPolicy.DESTROY,
 			websiteIndexDocument: 'index.html',
 			websiteErrorDocument: 'error.html'
 		})
+
+		const distribution = new cloudfront.CloudFrontWebDistribution(
+			this,
+			'PersonalWebsiteDistribution',
+			{
+				originConfigs: [
+					{
+						s3OriginSource: {
+							s3BucketSource: bucket
+						},
+						behaviors: [{isDefaultBehavior: true}]
+					}
+				]
+			}
+		)
 	}
 }
