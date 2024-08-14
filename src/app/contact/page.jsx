30c89e79b276c300d2e9923faa56d32d31d61062
Shane T. Geary <shane.geary@glowstik.com>
2024-08-14 10:37:26 -0600

    Expand contac buttons clickable area to entire circle

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,5 @@
+'use client'
 import Image from 'next/legacy/image'
-import Link from 'next/link'
 
 import styles from './contact.module.scss'
 
@@ -8,55 +8,53 @@ export default function Contact() {
 		<div className={styles['contactPageWrapper']}>
 			<div className={styles['contactTitleWrapper']}>Get in Touch</div>
 			<div className={styles['mediaLinksContainer']}>
-				<div className={styles['circleOne']}>
+				<div
+					className={styles['circleOne']}
+					onClick={() => {
+						window.open('https://www.linkedin.com/in/shanegeary/')
+					}}
+				>
 					<div className={styles['iconWrapper']}>
 						<Image alt='LinkedIn' src='/linkedin.png' layout='fill' />
 					</div>
-					<Link
-						className={styles['text']}
-						href={'https://www.linkedin.com/in/shanegeary/'}
-						target='_blank'
-					>
-						LinkedIn
-					</Link>
+					<div className={styles['text']}>LinkedIn</div>
 				</div>
-				<div className={styles['circleTwo']}>
+				<div
+					className={styles['circleTwo']}
+					onClick={() => {
+						window.open('https://github.com/Shane-Geary')
+					}}
+				>
 					<div className={styles['iconWrapper']}>
 						<Image alt='GitHub' src='/github-sign.png' layout='fill' />
 					</div>
-					<Link
-						className={styles['text']}
-						href={'https://github.com/Shane-Geary'}
-						target='_blank'
-					>
-						Github
-					</Link>
+					<div className={styles['text']}>Github</div>
 				</div>
-				<div className={styles['circleThree']}>
+				<div
+					className={styles['circleThree']}
+					onClick={() => {
+						window.open(
+							'https://www.upwork.com/freelancers/~01215676e614c48b8a?mp_source=share'
+						)
+					}}
+				>
 					<div className={styles['iconWrapper']}>
 						<Image alt='UpWork' src='/upwork.png' layout='fill' />
 					</div>
-					<Link
-						className={styles['text']}
-						href={
-							'https://www.upwork.com/freelancers/~01215676e614c48b8a?mp_source=share'
-						}
-						target='_blank'
-					>
-						UpWork
-					</Link>
+					<div className={styles['text']}>UpWork</div>
 				</div>
-				<div className={styles['circleFour']}>
+				<div
+					className={styles['circleFour']}
+					onClick={() => {
+						window.open(
+							'mailto:shanegeary1994@gmail.com?subject=Inquiry&body=Hi%20Shane,%0D%0A%0D%0A'
+						)
+					}}
+				>
 					<div className={styles['iconWrapper']}>
 						<Image alt='Gmail' src='/email.png' layout='fill' />
 					</div>
-					<Link
-						className={styles['text']}
-						href='mailto:shanegeary1994@gmail.com?subject=Inquiry&body=Hi%20Shane,%0D%0A%0D%0A'
-						target='_blank'
-					>
-						Gmail
-					</Link>
+					<div className={styles['text']}>Gmail</div>
 				</div>
 			</div>
 		</div>
