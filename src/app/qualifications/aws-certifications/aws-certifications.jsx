0ec141216687d9cc3c495b3d1e771eb25ad80783
Shane T. Geary <shane.geary@glowstik.com>
2024-08-14 09:53:00 -0600

    Re-design of aws certs section

--- src/app/qualifications/aws-certifications/aws-certifications.jsx
+++ src/app/qualifications/aws-certifications/aws-certifications.jsx
@@ -6,13 +6,25 @@ import AWSCCPCert from '../../../../public/aws-certified-cloud-practitioner.png'
 export default function AWSCerts() {
 	return (
 		<div className={styles['awsContainer']}>
-			<div className={styles['awsTextSection']}>
-				<div className={styles['awsText']}>
-					"Earners of this certification have a fundamental understanding of IT
-					services and their uses in the AWS Cloud. They demonstrated cloud
-					fluency and foundational AWS knowledge. Badge owners are able to
-					identify essential AWS services necessary to set up AWS-focused
-					projects."
+			<div className={styles['awsTextSectionWrapper']}>
+				<div className={styles['awsTextSection']}>
+					<div className={styles['awsText']}>
+						"Earners of this certification have a fundamental understanding of
+						IT services and their uses in the AWS Cloud. They demonstrated cloud
+						fluency and foundational AWS knowledge. Badge owners are able to
+						identify essential AWS services necessary to set up AWS-focused
+						projects."
+					</div>
+				</div>
+				<div
+					className={styles['verifyWrapper']}
+					onClick={() => {
+						window.open(
+							'https://www.credly.com/badges/b0cae331-62a6-4e80-8cf6-a62d5e2cf9a4/public_url'
+						)
+					}}
+				>
+					Verify Here
 				</div>
 			</div>
 			<div className={styles['awsCCPContainer']}>
