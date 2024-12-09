f166f5ae4c17899c38a23ea41ff14562200437e4
Shane T. Geary <shane.geary@glowstik.com>
2024-12-08 17:58:48 -0700

    WIP new design for qualif. components

--- src/app/qualifications/flatiron/page.jsx
+++ src/app/qualifications/flatiron/page.jsx
@@ -14,26 +14,26 @@ export default function Flatiron() {
 	return (
 		<div className={styles['flatironContainer']}>
 			<div className={styles['flatironTextSection']}>
+				<div
+					className={classNames(styles['flatironCertImageWrapper'], {
+						[styles['fullscreenImage']]: imageFullscreen
+					})}
+					onClick={() => {
+						setImageFullscreen(!imageFullscreen)
+					}}
+				>
+					<Image
+						alt='Flatiron Certificate'
+						src={FlatironCertificate}
+						// width={100}
+						// height={100}
+						layout='responsive'
+					/>
+				</div>
 				<div className={styles['flatironText']}>
 					Rigorous coding bootcamp where I learned the fundamentals of web
 					development languages. Such as HTML, CSS, JavaScript, ReactJS, etc.
 				</div>
-			</div>
-			<div
-				className={classNames(styles['flatironCertImageWrapper'], {
-					[styles['fullscreenImage']]: imageFullscreen
-				})}
-				onClick={() => {
-					setImageFullscreen(!imageFullscreen)
-				}}
-			>
-				<Image
-					alt='Flatiron Certificate'
-					src={FlatironCertificate}
-					// width={100}
-					// height={100}
-					layout='responsive'
-				/>
 			</div>
 		</div>
 	)
