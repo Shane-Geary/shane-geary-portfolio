ece53520c76846748bb5f1106bdb4d3cdedfc1f8
Shane T. Geary <shane.geary@glowstik.com>
2025-07-05 08:16:20 -0600

    Updated responsive styles for Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,54 +7,56 @@ export default function Contact() {
 	return (
 		<div className={styles['contactPageWrapper']}>
 			<div className={styles['contactTitleWrapper']}>Get in Touch</div>
-			<div className={styles['mediaLinksContainer']}>
-				<div
-					className={styles['circleOne']}
-					onClick={() => {
-						window.open('https://www.linkedin.com/in/shanegeary/')
-					}}
-				>
-					<div className={styles['iconWrapper']}>
-						<Image alt='LinkedIn' src='/linkedin.png' layout='fill' />
+			<div className={styles['mediaLinksPositionWrapper']}>
+				<div className={styles['mediaLinksContainer']}>
+					<div
+						className={styles['circleOne']}
+						onClick={() => {
+							window.open('https://www.linkedin.com/in/shanegeary/')
+						}}
+					>
+						<div className={styles['iconWrapper']}>
+							<Image alt='LinkedIn' src='/linkedin.png' layout='fill' />
+						</div>
+						<div className={styles['text']}>LinkedIn</div>
 					</div>
-					<div className={styles['text']}>LinkedIn</div>
-				</div>
-				<div
-					className={styles['circleTwo']}
-					onClick={() => {
-						window.open('https://github.com/Shane-Geary')
-					}}
-				>
-					<div className={styles['iconWrapper']}>
-						<Image alt='GitHub' src='/github-sign.png' layout='fill' />
+					<div
+						className={styles['circleTwo']}
+						onClick={() => {
+							window.open('https://github.com/Shane-Geary')
+						}}
+					>
+						<div className={styles['iconWrapper']}>
+							<Image alt='GitHub' src='/github-sign.png' layout='fill' />
+						</div>
+						<div className={styles['text']}>Github</div>
 					</div>
-					<div className={styles['text']}>Github</div>
-				</div>
-				<div
-					className={styles['circleThree']}
-					onClick={() => {
-						window.open(
-							'https://www.upwork.com/freelancers/~01215676e614c48b8a?mp_source=share'
-						)
-					}}
-				>
-					<div className={styles['iconWrapper']}>
-						<Image alt='UpWork' src='/upwork.png' layout='fill' />
+					<div
+						className={styles['circleThree']}
+						onClick={() => {
+							window.open(
+								'https://www.upwork.com/freelancers/~01215676e614c48b8a?mp_source=share'
+							)
+						}}
+					>
+						<div className={styles['iconWrapper']}>
+							<Image alt='UpWork' src='/upwork.png' layout='fill' />
+						</div>
+						<div className={styles['text']}>UpWork</div>
 					</div>
-					<div className={styles['text']}>UpWork</div>
-				</div>
-				<div
-					className={styles['circleFour']}
-					onClick={() => {
-						window.open(
-							'mailto:shanegeary1994@gmail.com?subject=Inquiry&body=Hi%20Shane,%0D%0A%0D%0A'
-						)
-					}}
-				>
-					<div className={styles['iconWrapper']}>
-						<Image alt='Gmail' src='/email.png' layout='fill' />
+					<div
+						className={styles['circleFour']}
+						onClick={() => {
+							window.open(
+								'mailto:shanegeary1994@gmail.com?subject=Inquiry&body=Hi%20Shane,%0D%0A%0D%0A'
+							)
+						}}
+					>
+						<div className={styles['iconWrapper']}>
+							<Image alt='Gmail' src='/email.png' layout='fill' />
+						</div>
+						<div className={styles['text']}>Gmail</div>
 					</div>
-					<div className={styles['text']}>Gmail</div>
 				</div>
 			</div>
 		</div>
