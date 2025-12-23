e5ca2ac3354e35120f1b8c7bd2f0547f649373ee
Shane T. Geary <shane.geary@glowstik.com>
2025-12-23 14:43:38 -0700

    New design for name title hero image

--- src/components/server-side-renders/name-title-image.jsx
+++ src/components/server-side-renders/name-title-image.jsx
@@ -1,15 +1,7 @@
 import Image from 'next/legacy/image'
 
-import nameTitleImage from '../../../public/NameTitleImage.png'
+import nameTitleImage from '../../../public/name-title.png'
 
 export const NameTitleImage = () => {
-	return (
-		<Image
-			alt='Shane T Geary'
-			src={nameTitleImage}
-			// width={'100%'}
-			// height={100}
-			layout='responsive'
-		/>
-	)
+	return <Image alt='Shane T Geary' src={nameTitleImage} layout='responsive' />
 }
